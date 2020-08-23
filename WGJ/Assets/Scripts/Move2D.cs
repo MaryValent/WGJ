2678d8319690e1e8f8131e505a9a3caceeb281d5
Valentina <midnanakamura@gmail.com>
2020-08-23 13:48:06 -0300

    jogador movimenta
    movimentação do personagem é funcional, ele pula e tudo mais

---
+++ WGJ/Assets/Scripts/Move2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+    public bool isGrounded = false;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.fixedDeltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+}
