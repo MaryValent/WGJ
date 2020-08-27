df44386500b0ce73ce4c635b02ed0095b2f1ddde
Valentina <midnanakamura@gmail.com>
2020-08-26 22:13:16 -0300

    coletaveis e menu inicial
    menu inicial agora funciona com a imagem que deveria, com os quadros brilhando no hover do mouse
    
    o player coleta as penas e emite o som, mas está bugado no contador da tela

---
+++ WGJ/Assets/Scripts/CollectPenas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectPenas : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collectSound.Play();
+        ScoreSystem.Score += 1;
+        Destroy(gameObject);
+    }
+}
---
+++ WGJ/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int Score;
+
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "x " + Score;
+    }
+
+
+}
